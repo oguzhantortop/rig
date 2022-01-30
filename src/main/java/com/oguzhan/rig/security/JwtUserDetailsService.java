a914862eb7d2022fc36382ab93023a4f33a9a6e8
oguzhan.tortop@softtech.com.tr <oguzhan.tortop@softtech.com.tr>
2022-01-31 00:27:43 +0300

    secure api completed

---
+++ src/main/java/com/oguzhan/rig/security/JwtUserDetailsService.java
@@ -0,0 +1,37 @@
+package com.oguzhan.rig.security;
+
+import java.util.ArrayList;
+
+import com.oguzhan.rig.dto.CustomerDto;
+import com.oguzhan.rig.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+    @Autowired
+    CustomerService customerService;
+
+    @Autowired
+    private PasswordEncoder bcryptEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        CustomerDto customer =null;
+        try {
+            customer = customerService.getCustomer(Long.valueOf(username));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (customer == null) {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
+        return new org.springframework.security.core.userdetails.User(customer.getId().toString(), bcryptEncoder.encode(customer.getPassword()),
+                new ArrayList<>());
+    }
+}
