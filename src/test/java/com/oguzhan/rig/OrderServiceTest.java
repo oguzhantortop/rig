679afe78e440d4a4a6fc4e35a42098b56e3469b1
oguzhan.tortop@softtech.com.tr <oguzhan.tortop@softtech.com.tr>
2022-01-31 14:00:54 +0300

    orderservicetest fixed

--- src/test/java/com/oguzhan/rig/OrderServiceTest.java
+++ src/test/java/com/oguzhan/rig/OrderServiceTest.java
@@ -1,65 +1,95 @@
 package com.oguzhan.rig;
 
-import com.oguzhan.rig.dto.OrderDto;
+import com.oguzhan.rig.dao.Book;
+import com.oguzhan.rig.dao.Customer;
+import com.oguzhan.rig.dto.*;
 import com.oguzhan.rig.exception.BusinessException;
 import com.oguzhan.rig.exception.ResourceNotFound;
+import com.oguzhan.rig.service.BookService;
+import com.oguzhan.rig.service.CustomerService;
 import com.oguzhan.rig.service.OrderService;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;EnableAutoConfiguration
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@EnableAutoConfiguration
 @SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class OrderServiceTest {
     @Autowired
     OrderService orderService;
 
+    @Autowired
+    CustomerService customerService;
+
+    @Autowired
+    BookService bookService;
+
+    private CustomerDto customer;
+    private BookDto book;
+
+    @BeforeAll
+    void createOrderTestData() throws Exception{
+        CustomerDto customerDto = new CustomerDto();
+        customerDto.setName("John");
+        customerDto.setSurname("Doe");
+        customerDto.setEmail("ordertest@gmail.com");
+        customerDto.setPassword("password");
+        this.customer = customerService.createCustomer(customerDto);
+
+        BookDto bookDto = new BookDto();
+        bookDto.setAuthor("John Doe");
+        bookDto.setIsbn("123456789");
+        bookDto.setTitle("Book of Order");
+        bookDto.setStockCount(10L);
+        bookDto.setUnitPrice(BigDecimal.valueOf(2.99));
+        this.book = bookService.createBook(bookDto);
+    }
+
     @Test
     @DisplayName("Create Order Service Test")
     void testCreateOrder() throws Exception{
-        OrderDto orderDto = getOrderDto();
-        OrderDto order = orderService.createOrder(orderDto);
+        OrderRequestDto orderDto = getOrderRequestDto();
+        OrderResponseDto order = orderService.createOrder(orderDto);
         assertAll(
                 () -> assertNotNull(order),
                 () -> assertNotNull(order.getId()),
-                () -> assertEquals(order.getName(),"John"),
-                () -> assertEquals(order.getSurname(),"Doe"),
-                () -> assertEquals(order.getEmail(),"john.doe@gmail.com"),
-                () -> assertEquals(order.getPassword(),"password"),
-                () -> assertThrows(BusinessException.class,() -> orderService.createOrder(orderDto))
+                () -> assertEquals(order.getTotalPrice(),book.getUnitPrice().doubleValue()*orderDto.getOrderDetailList().get(0).getCount().longValue()),
+                () -> assertNotNull(order.getCustomer()),
+                () -> assertNotNull(order.getTotalPrice()),
+                () -> assertNotNull(order.getOrderDetailList())
         );
     }
 
-    private OrderDto getOrderDto() throws Exception {
-        OrderDto orderDto = new OrderDto();
-        orderDto.setName("John");
-        orderDto.setSurname("Doe");
-        orderDto.setEmail("john.doe@gmail.com");
-        orderDto.setPassword("password");
+    private OrderRequestDto getOrderRequestDto() throws Exception {
+        OrderRequestDto orderDto = new OrderRequestDto();
+        orderDto.setOrderDate(Calendar.getInstance().getTime());
+        orderDto.setCustomerId(customer.getId());
+        List<OrderDetailReqDto> orderDetailList = new ArrayList<>();
+        OrderDetailReqDto orderDetailReqDto = new OrderDetailReqDto();
+        orderDetailReqDto.setBookId(book.getId());
+        orderDetailReqDto.setCount(3L);
+        orderDetailList.add(orderDetailReqDto);
+        orderDto.setOrderDetailList(orderDetailList);
         return orderDto;
-    }
-
-
-    @Test
-    @DisplayName("Delete Order Service Test")
-    void testDeleteOrder() throws Exception{
-        assertThrows(ResourceNotFound.class,() -> orderService.deleteOrder(-1L));
-    }
-
-    @Test
-    @DisplayName("Update Order Service Test")
-    void testUpdateOrder() throws Exception{
-        OrderDto orderDto = getOrderDto();
-        assertThrows(ResourceNotFound.class,() -> orderService.updateOrder(orderDto,-1L));
     }
 
     @Test
     @DisplayName("Get Order Service Test")
     void testGetOrder() throws Exception{
-        OrderDto orderDto = getOrderDto();
-        orderDto.setEmail("abc@abc.com");
-        OrderDto order = orderService.createOrder(orderDto);
+        OrderRequestDto orderDto = getOrderRequestDto();
+        OrderResponseDto order = orderService.createOrder(orderDto);
         assertAll(
                 () -> assertThrows(ResourceNotFound.class,() -> orderService.getOrder(-1L)),
                 () -> assertNotNull(orderService.getOrder(order.getId()))
